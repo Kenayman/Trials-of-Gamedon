63d9764cf7720a1dcc9e753c562daabf2ddbac03
Kenneth Arguelles Camarena <kenneth.camarena7207@alumnos.udg.mx>
2023-04-18 01:07:30 -0600

    Better Movement
    Implemented player dash and reorganized code for the controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,18 +8,14 @@ public class PlayerController : MonoBehaviour
     private Animator animator;
     private SpriteRenderer sprite;
     private BoxCollider2D coll;
-    private float dashPower = 24f;
-    private bool canDash=true;
-    private float dashCd=1f;
-    private float lastDashTime = -1f;
-
+    private float direction;
+    private PlayerDash playerDash;
+    public float Direction => direction;
 
     [SerializeField] private float jumpSpeed = 10f;
     [SerializeField] private float moveForce;
     [SerializeField] private LayerMask jumpable;
     [SerializeField] private float bounceSpeed;
-
-    private bool isPrep = false;
 
     private bool jumped= false;
 
@@ -28,21 +24,35 @@ public class PlayerController : MonoBehaviour
     private enum MovementState { idle, running, jumping, falling, prep, combatIdle}
     private MovementState state = MovementState.idle;
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
         playerRb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         sprite = GetComponent<SpriteRenderer>();
         coll = GetComponent<BoxCollider2D>();
+        playerDash = GetComponent<PlayerDash>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Controls();
+        if(!playerDash.IsDashing)
+        {
+            Jump();
+        }
+
     }
 
-    private void Controls()
+    private void FixedUpdate()
+    {
+        if(!playerDash.IsDashing)
+        {
+            Move();
+        }
+
+    }
+
+    private void Jump()
     {
         MovementState state = this.state;
 
@@ -57,13 +67,9 @@ public class PlayerController : MonoBehaviour
             StartCoroutine(ResetGravity());
         }
 
-        if (Input.GetKey(KeyCode.LeftShift) && canDash)
-        {
-            StartCoroutine(Dash());
-        }
-
-
-
+    }
+    private void Move()
+    {
         float horizontalInput = Input.GetAxisRaw("Horizontal");
         playerRb.velocity = new Vector2(horizontalInput * moveForce, playerRb.velocity.y);
 
@@ -71,14 +77,17 @@ public class PlayerController : MonoBehaviour
         {
             state = MovementState.running;
             sprite.flipX = false;
-            
-            
+            direction = 1;
+
+
         }
 
         else if (horizontalInput < 0f)
         {
             state = MovementState.running;
             sprite.flipX = true;
+            direction = -1;
+
         }
         else
         {
@@ -95,16 +104,6 @@ public class PlayerController : MonoBehaviour
         }
 
 
-        if (Input.GetKeyDown(KeyCode.Z))
-        {
-            isPrep = true;
-            state = MovementState.prep;
-        }
-        if (isPrep && animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
-        {
-            isPrep = false;
-            state = MovementState.combatIdle;
-        }
 
         animator.SetInteger("state", (int)state);
     }
@@ -122,35 +121,6 @@ public class PlayerController : MonoBehaviour
         playerRb.gravityScale = 3f; // restaura la gravedad a su valor original
     }
 
-    private IEnumerator Dash()
-    {
-        float dashDuration = 0.5f;
-        float dashStartTime = Time.time;
-
-        playerRb.velocity = new Vector2(transform.localScale.x * dashPower, 0f);
-        animator.SetTrigger("Dash");
-
-        // Establecer la última vez que se usó el dash
-        lastDashTime = Time.time;
-
-        while (Time.time < dashStartTime + dashDuration)
-        {
-            // Detectar si hay algo debajo del personaje
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1f, jumpable);
-            if (hit.collider != null)
-            {
-                // Si hay algo debajo, establecer la velocidad y del personaje en 0
-                playerRb.velocity = new Vector2(playerRb.velocity.x, 0f);
-            }
-            else
-            {
-                // Si no hay nada debajo, establecer la velocidad y del personaje en un valor negativo para que comience a caer
-                playerRb.velocity = new Vector2(playerRb.velocity.x, -10f);
-            }
-
-            yield return null;
-        }
-    }
 
     public void Bounce()
     {
---
+++ Assets/Scripts/PlayerDash.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDash : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private PlayerController player;
+    private float baseGravity;
+    private Animator animator;
+
+
+    [SerializeField] private float dashTime =0.2f;
+    [SerializeField] private float dashSpeed = 20;
+    [SerializeField] private float timeCanDash;
+
+    private bool isDashing;
+    private bool canDash = true;
+    public bool IsDashing=> isDashing;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        player = GetComponent<PlayerController>();
+        rb = GetComponent<Rigidbody2D>();
+        baseGravity = rb.gravityScale;
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftShift) && canDash) 
+        {
+            StartCoroutine(Dash());
+            animator.SetTrigger("Dash");
+        }
+    }
+
+    private IEnumerator Dash()
+    {
+        isDashing = true;
+        canDash = false;
+        rb.gravityScale = 0f;
+        rb.velocity = new Vector2(dashSpeed * player.Direction, rb.velocity.y);
+
+        yield return new WaitForSeconds(dashTime);
+
+        isDashing = false;
+        rb.gravityScale = baseGravity;
+
+        yield return new WaitForSeconds(timeCanDash);
+        canDash = true;
+
+
+
+    }
+}
